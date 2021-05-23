47ac699ec3e3dd75fb5e0bca1c7a1cc87da72742
Minebruke <80499695+Minebruke@users.noreply.github.com>
2021-05-23 17:16:44 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>My test work</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+			<section id="header" class="dark">
+				<header>
+					<h1>Welcome to SCA-18</h1>
+					<p>My work for you its me Shakirov Eldiyar</p>
+				</header>
+				<footer>
+					<a href="#first" class="button scrolly">next</a>
+				</footer>
+			</section>
+
+			<section id="first" class="main">
+				<header>
+					<div class="container">
+						<h2>адаптивный сайт можете проверить на телефонной и планшетной версии Жавлон агай</h2>
+						<p> Не важно же что написано на сайте, главное сайт работает адаптивно и согласно курсу на котором были задания<br />
+						</p>
+					</div>
+				</header>
+				<div class="content dark style1 featured">
+					<div class="container">
+						<div class="row">
+							<div class="col-4 col-12-narrow">
+								<section>
+									<span class="feature-icon"><span class="icon fa-clock"></span></span>
+									<header>
+										<h3>Time</h3>
+									</header>
+									<p></p>
+								</section>
+							</div>
+							<div class="col-4 col-12-narrow">
+								<section>
+									<span class="feature-icon"><span class="icon solid fa-bolt"></span></span>
+									<header>
+										<h3>lightning</h3>
+									</header>
+									<p></p>
+								</section>
+							</div>
+							<div class="col-4 col-12-narrow">
+								<section>
+									<span class="feature-icon"><span class="icon solid fa-cloud"></span></span>
+									<header>
+										<h3>cloud</h3>
+									</header>
+									<p></p>
+								</section>
+							</div>
+							<div class="col-12">
+								<footer>
+									<a href="#second" class="button scrolly">next</a>
+								</footer>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		
+			<section id="second" class="main">
+				<header>
+					<div class="container">
+						<h2>Конец сайта</h2>
+						<p><br />
+						</p>
+					</div>
+				</header>
+				
+			<section id="fourth" class="main">
+				<header>
+					<div class="container">
+						<h2>Форма заполнения</h2>
+						<p><br />
+						</p>
+					</div>
+				</header>
+				<div class="content style4 featured">
+					<div class="container medium">
+						<form method="post" action="#">
+							<div class="row gtr-50">
+								<div class="col-6 col-12-mobile"><input type="text" placeholder="Name" /></div>
+								<div class="col-6 col-12-mobile"><input type="text" placeholder="Email" /></div>
+								<div class="col-12"><textarea name="message" placeholder="Message"></textarea></div>
+								<div class="col-12">
+									<ul class="actions special">
+										<li><input type="submit" class="button" value="Send Message" /></li>
+										<li><input type="reset" class="button alt" value="Clear Form" /></li>
+									</ul>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</section>
+
+		
+			<section id="footer">
+				<ul class="icons">
+					<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+					<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+					<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+					<li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>
+					<li><a href="#" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+				</ul>
+				<div class="copyright">
+					<ul class="menu">
+						<li>&copy; Shakirov Eldiyar SCA-18</li>
+					</ul>
+				</div>
+			</section>
+
+		
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
